21880898d03e90bfee6dbd814e83b4bbe655c064
Hugo Ferreira <hugopalmaferreira@gmail.com>
2019-10-02 21:04:12 +0100

    Add statistics to LMD and LMJM

--- RetrievalModelsMatrix.py
+++ RetrievalModelsMatrix.py
@@ -8,9 +8,9 @@ class RetrievalModelsMatrix:
         self.tf = tf
 
         ## VSM statistics
-        self.term_doc_freq = np.sum(tf != 0, axis=0)
-        self.term_coll_freq = np.sum(tf, axis=0)
-        self.docLen = np.sum(tf, axis=1)
+        self.term_doc_freq = np.sum(tf != 0, axis=0)#term collection freq
+        self.term_coll_freq = np.sum(tf, axis=0) #term collection freq
+        self.docLen = np.sum(tf, axis=1) #document size
 
         idf_value = np.log(np.size(tf, axis = 0) / self.term_doc_freq) 
         self.idf = (idf_value > 0.01) * idf_value
@@ -19,7 +19,9 @@ class RetrievalModelsMatrix:
         self.docNorms = np.sqrt(np.sum(np.power(self.tfidf, 2), axis=1))
 
         ## LMD statistics
-
+        self.collection_size = np.sum(self.docLen)
+        self.term_coll_freq_prob = np.divide(self.term_coll_freq / self.collection_size)
+        self.term_doc_freq_prob = self.tf/np.array(self.docLen)[:, None]
 
         ## LMJM statistics
 
@@ -36,6 +38,8 @@ class RetrievalModelsMatrix:
         return doc_scores
 
     def score_lmd(self, query):
+        query_vector = self.vectorizer.transform([query]).toarray()
+        indexes = np.where(query_vector[0] != 0)[0]
         
         return doc_scores
 
